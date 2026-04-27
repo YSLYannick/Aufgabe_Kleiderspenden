292d6f9b379baf7cda497b6143c309e661dc9afe
Yannick <y.richner@web.de>
2026-04-27 20:46:18 +0200

    PLZ-Prüfung und Fehlermeldung implementiert

--- js/script.js
+++ js/script.js
@@ -21,3 +21,44 @@ modus_aktualisieren();
 radios.forEach(function(radio) {
     radio.addEventListener('change', modus_aktualisieren);
 });
+
+/* PLZ Prüfen */
+const plzInput = document.getElementById('plz');
+const plzFehler = document.getElementById('plz-fehler');
+
+/* ersten zwei Ziffern der Geschäftsstelle-PLZ */
+const geschaeftsstelle_plz = '67';
+
+function plz_pruefen() {
+
+    /* Eingabe des Nutzers holen */
+    const eingabe = plzInput.value;
+
+    if (eingabe.length === 5) {
+        const ersten_zwei = eingabe.substring(0, 2);
+    
+        if (ersten_zwei !== geschaeftsstelle_plz) {
+            plzInput.classList.add('fehler');
+            plzFehler.classList.add('sichtbar');
+        } else {
+            plzInput.classList.remove('fehler');
+            plzFehler.classList.remove('sichtbar');            
+        }
+    } else {
+        plzInput.classList.remove('fehler');
+        plzFehler.classList.remove('sichtbar');
+    }
+}
+
+plzInput.addEventListener('input', plz_pruefen);
+
+/* Formular abfangen */
+const formular = document.getElementById('spenden-formular');
+
+formular.addEventListener ('submit', function(event) {
+    
+    /* Seite neu laden nach abschicken des Formulars verhindern */
+    event.preventDefault();
+
+    console.log("Formular abgeschickt")
+});
